bce356811d19cfdc04c6336f286a018db2112d34
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-07-05 09:57:18 +0700

    Initial Commit

---
+++ MathAppBasicTests/MathAppBasicTests.swift
@@ -0,0 +1,16 @@
+//
+//  MathAppBasicTests.swift
+//  MathAppBasicTests
+//
+//  Created by Tuấngg Anhhh on 5/7/25.
+//
+
+import Testing
+
+struct MathAppBasicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
