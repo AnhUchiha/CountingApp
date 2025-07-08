daab5b9387aa86b1e26aaa17fc3c6cb7370cccfb
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-07-08 20:43:19 +0700

    global view

---
+++ CountingApp/Views/GearIconView.swift
@@ -0,0 +1,18 @@
+//
+//  GearIconView.swift
+//  CountingApp
+//
+//  Created by Tuấngg Anhhh on 8/7/25.
+//
+
+import SwiftUI
+
+struct GearIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GearIconView()
+}
